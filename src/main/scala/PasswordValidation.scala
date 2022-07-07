643b1abb013e2a7c0170edb702e55ee8aa7d8882
Sergio Flor <sflor5@alumno.uned.es>
2022-07-07 10:32:43 +0200

    green test for Iteration 1

--- src/main/scala/PasswordValidation.scala
+++ src/main/scala/PasswordValidation.scala
@@ -1,3 +1,9 @@
 object PasswordValidation {
-  def validate(password: String): Boolean = false
+  def validate(password: String): Boolean =
+    password.length > 8 &&
+      password.contains("_") &&
+      password.exists(_.isDigit) &&
+      password.exists(_.isUpper) &&
+      password.exists(_.isLower)
+
 }
