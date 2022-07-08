89aaf3628a5cade87808f7eae65f7fc06d5bcfda
Sergio Flor <sflor5@alumno.uned.es>
2022-07-08 10:16:57 +0200

    Iteration 2 - green test

--- src/main/scala/PasswordValidation.scala
+++ src/main/scala/PasswordValidation.scala
@@ -7,12 +7,15 @@ object PasswordValidation {
       password.exists(_.isLower)
 
   sealed abstract class Validations(length: Int, capital: Boolean, lowercase: Boolean, number: Boolean, underscore: Boolean) {
-    def validate(password: String): Boolean =
-      password.length > 8 &&
-        password.contains("_") &&
-        password.exists(_.isDigit) &&
-        password.exists(_.isUpper) &&
-        password.exists(_.isLower)
+    def validate(password: String): Boolean = {
+    (password.length > length) &&
+       (if (underscore) password.contains("_") else true ) &&
+       (if (number) password.exists(_.isDigit) else true) &&
+       (if (capital) password.exists(_.isUpper) else true) &&
+        (if (lowercase) password.exists(_.isLower) else true)
+    }
+
+    override def toString: String = s"Validación 2: length=$length, capital=$capital, lower=$lowercase, number=$number, _=$underscore"
   }
 
   final case object Validation2 extends Validations(6, true, true, true, false)
