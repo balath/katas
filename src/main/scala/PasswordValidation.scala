5b0c5302cbeae7fc843f1512819739403f050f53
Sergio Flor <sflor5@alumno.uned.es>
2022-07-08 09:59:11 +0200

    Iteration 2 - red test

--- src/main/scala/PasswordValidation.scala
+++ src/main/scala/PasswordValidation.scala
@@ -6,4 +6,17 @@ object PasswordValidation {
       password.exists(_.isUpper) &&
       password.exists(_.isLower)
 
+  sealed abstract class Validations(length: Int, capital: Boolean, lowercase: Boolean, number: Boolean, underscore: Boolean) {
+    def validate(password: String): Boolean =
+      password.length > 8 &&
+        password.contains("_") &&
+        password.exists(_.isDigit) &&
+        password.exists(_.isUpper) &&
+        password.exists(_.isLower)
+  }
+
+  final case object Validation2 extends Validations(6, true, true, true, false)
+  final case object Validation3 extends Validations(16, true, true, false, true)
+
+
 }
